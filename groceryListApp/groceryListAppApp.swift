053e91f5af153f62a8d1aaa4cf4efbd699c0ab96
Javonne Johnson <jjavonne13@gmail.com>
2025-08-07 23:56:04 -0400

    Initial Commit

---
+++ groceryListApp/groceryListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  groceryListAppApp.swift
+//  groceryListApp
+//
+//  Created by Javonne Johnson on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct groceryListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
